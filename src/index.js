1d87a128719b71f985218fd32ab07c4d36ff0b7d
yzubr <vadzim.vinnikau@gmail.com>
2022-08-07 20:18:39 +0300

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let arr = ('' + Math.abs(n)).split('');
+    arr.reverse();
+    let res = arr.reduce(function (a, b) {
+        return a.concat(b)
+    });
+    return res
 }
